70381596c8d6e22110c95ae45f085af897942ead
Michael Kuo <tylpk1216@gmail.com>
2021-12-24 15:51:55 +0800

    Update arp.go

--- arp.go
+++ arp.go
@@ -7,20 +7,15 @@ import (
     "github.com/mdlayher/arp"
 )
 
-/*
-Index is 1, HardwareAddr is 00:00:00:00:00:00
-1: lo: <LOOPBACK,UP,LOWER_UP> mtu 65536 qdisc noqueue state UNKNOWN group default qlen 1000
-    link/loopback 00:00:00:00:00:00 brd 00:00:00:00:00:00
-    inet 127.0.0.1/8 scope host lo
-       valid_lft forever preferred_lft forever
-*/
 func main() {
-    // Use ip addr on Ubuntu 20.04 to find the interface of sender.
-    ifi := &net.Interface{
-        Index: 2,
-        HardwareAddr: net.HardwareAddr{0xaa, 0xbb, 0xcc, 0xdd, 0xee, 0xff},
+    // Use ip addr on Ubuntu 20.04 to find the interface name of sender.
+    ifi, err := net.InterfaceByName("enp3s0")
+	if err != nil {
+	    fmt.Println(err)
+	    return
 	}
-
+    }
+	
     c, err := arp.Dial(ifi)
     if err != nil {
         fmt.Println(err)
